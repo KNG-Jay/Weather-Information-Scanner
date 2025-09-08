9af1040c62e0a7623009f3aef81abcab5035e31c
KNG-JAY <jaylenjholloway@gmail.com>
2025-09-07 17:05:26 -0700

    Framework Established: missing callAPI functionality & switch statement.

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# This project requires OpenWeather's API in order to run "Out the box".
+
+# The API Key and Default Location files are found in the resources folder, after initial setup.
+
+# API Response is as follows:
+    
--- src/main/java/com/wis/WeatherScanner.java
+++ src/main/java/com/wis/WeatherScanner.java
@@ -1,23 +1,60 @@
 package main.java.com.wis;
 
 import java.util.Scanner;
+import java.util.Map;
+import java.util.HashMap;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+
 public class WeatherScanner {
 
+    private static String locationDefault;
+    private static String location;
+    private static String city;
+    private static String state;
+    private static String country;
+    private static String apiKey;
+    private static Map<String, String> apiResponse = new HashMap<>();
 
-    public String[] getInfo() {
+    WeatherScanner() {
+        apiKey = "Undefined";
+        locationDefault = "Undefined";
+    }
+
+    WeatherScanner(String key, String loc) {
+        apiKey = key;
+        locationDefault = location;
+    }
+
+
+    public String[] callAPI(String location, String key) {
+        this.decodeLocation(location);
+        
+
+    }
+
+
+    public void decodeLocation(String location) {
+        Scanner scnr = new Scanner(location);
+        city = scnr.next();
+        state = scnr.next();
+        country = scnr.next();
+        scnr.close();
+    }
+
+
+    public static String[] getInfo() {
         String[] info = new String[2];
-        info[0] = this.getAPIKey();
-        info[1] = this.getLocation();
+        info[0] = getAPIKey();
+        info[1] = getDefaultLocation();
         return info;
     }
 
 
-    private String getAPIKey() {
+    private static String getAPIKey() {
         Path filepath = Paths.get("src/main/java/resources/api-key.txt");
         String fileContent = "";
 
@@ -34,12 +71,9 @@ public class WeatherScanner {
     }
 
 
-    private String getLocation() {
+    private static String getDefaultLocation() {
         Path filepath = Paths.get("src/main/java/resources/location-default.txt");
         String fileContent = "";
-        String city;
-        String state;
-        String country;
 
         try {
             fileContent = Files.readString(filepath);
@@ -47,7 +81,8 @@ public class WeatherScanner {
             city = scnr_in.next();
             state = scnr_in.next();
             country = scnr_in.next();
-            System.out.printf("Location Set As: \nCity: %s\nState/Province: %s\nCountry: %s\n", city, state, country);
+            System.out.printf("Local Location Set As: \nCity: %s\nState/Province: %s\nCountry: %s\n",
+                 city, state, country);
             scnr_in.close();
         } catch (IOException e) {
             System.err.println("Error reading file: " + e.getMessage());
@@ -59,7 +94,12 @@ public class WeatherScanner {
     }
 
 
-    private void setAPIKey(String key) {
+    public String getLocation() {
+        return location;
+    }
+
+
+    private static void setAPIKey(String key) {
         Path filepath = Paths.get("src/main/java/resources/api-key.txt");
 
         try {
@@ -70,32 +110,99 @@ public class WeatherScanner {
             e.printStackTrace();
         }
 
+        System.out.println("API Key Set!");
+
     }
 
 
-    private void setLocation(String city, String state, String country) {
+    private static void setDefaultLocation(String city, String state, String country) {
         Path filepath = Paths.get("src/main/java/resources/location-default.txt");
-        String location = filepath.toString();
+        location = filepath.toString();
 
         try {
             Files.write(filepath, location.getBytes());
             System.out.println("Writing: " + location.toString() + "\nTo: " + filepath);
+            locationDefault = location;
         } catch (IOException e) {
             System.err.println("Error Writing Location to File: " + e.getMessage());
             e.printStackTrace();
         }
 
+        System.out.println("Default Location Set!");
+
     }
+
+
+    private void setLocation(String city, String state, String country) {
+        location = String.format("%s %s %s", city, state, country);
+    }
+
+
+    public static void tui() {
+        Scanner scnr = new Scanner(System.in);
+        String answer;
+        char ch = ' ';
+        WeatherScanner weatherScanner = new WeatherScanner(getInfo()[0], getInfo()[1]);
+        /*TODO: Have user use saved key/location
+         or input api-key/ location and save to: 
+         ../resources/api-key.txt
+         ../resources/location-default.txt
+        */
+        while (ch != 'Q') {
+
+            System.out.println("----------  Weather Scanner System  ----------");
+            System.out.println("A Tool That Allows You To Check Local/Distant Weather Data!");
+            System.out.println("ENTER 'q' TO QUIT!\n");
+            getInfo();
+            
+            if (WeatherScanner.apiKey == null || WeatherScanner.apiKey == "Undefined") {
+                System.out.println("Please Enter an API Key From openweathermap.org:");
+                answer = scnr.next();
+                if (answer.length() == 1) {
+                    ch = answer.toUpperCase().charAt(0);
+                }
+                setAPIKey(answer);
+            }
+
+            if (WeatherScanner.locationDefault == null || WeatherScanner.locationDefault == "Undefined") {
+                System.out.println("Please Enter A Default Location:");
+                System.out.print("City: ");
+                city = scnr.nextLine();
+                if (city.length() == 1) {
+                    ch = city.toUpperCase().charAt(0);
+                }
+                System.out.print("State/Province: ");
+                state = scnr.nextLine();
+                System.out.print("Country: ");
+                country = scnr.nextLine();
+                setDefaultLocation(city, state, country);
+            }
+
+            apiKey = getAPIKey();
+            locationDefault = getDefaultLocation();
+
+            switch (ch) {
+                case 'l':
+                    getInfo();
+                    break;
+            
+                default:
+                    break;
+
+            }
+
+        }
+
+        scnr.close();
+
+    }
+
 
     public static void main(String[] args) {
 
-        WeatherScanner weatherScanner = new WeatherScanner();
-        weatherScanner.getInfo();
-        /*TODO: Have user use saved key
-         or input api key and save to: 
-         ../resources/api-key.txt
-        */
+        tui();
 
     }
+
 
 }
